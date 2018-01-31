1249465b66935d8587d7f27b732f5786f32a8b75
sahandMg <sahand.mg.ne@gmail.com>
2018-01-31 16:00:44 +0330

    first commit

---
+++ resources/views/CheckList.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fa">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+	<title>Index 2</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<body>
+
+   <div class="container">
+
+
+
+
+       <table class="table table-striped table-bordered ">
+           <thead>
+           <tr>
+               <th class="text-center">وضعیت</th>
+               <th class="text-center">نام گروه</th>
+               <th class="text-center">نام شرکت کننده </th>
+           </tr>
+           </thead>
+           <tbody>
+
+
+
+               <tr>
+                   <td  class="text-center"><span class="glyphicon glyphicon-unchecked"></span></td>
+
+                   <td  class="text-center">sahand</td>
+                   <td  class="text-center">sahand</td>
+
+               </tr>
+
+               <tr>
+                   <td  class="text-center"><span class="glyphicon glyphicon-unchecked"></span></td>
+
+                   <td  class="text-center">sahand</td>
+                   <td  class="text-center">sahand</td>
+
+               </tr>
+
+           </tbody>
+       </table>
+
+
+   </div>
+
+
+
+</body>
+</html>
